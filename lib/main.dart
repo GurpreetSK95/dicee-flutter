c1dd668afeb8d4b3109c00802c07c1dc8b88386a
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-04-12 02:47:15 +0530

    Setup dice roll on dice click.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<DicePage> {
+  final random = Random();
+  int firstDiceOutcome = 1;
+  int secondDiceOutcome = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("images/dice$firstDiceOutcome.png"),
+              onPressed: () {
+                setState(() {
+                  rollDice();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rollDice();
+                });
+              },
+              child: Image.asset("images/dice$secondDiceOutcome.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void rollDice() {
+    firstDiceOutcome = random.nextInt(6) + 1;
+    secondDiceOutcome = random.nextInt(6) + 1;
   }
 }
